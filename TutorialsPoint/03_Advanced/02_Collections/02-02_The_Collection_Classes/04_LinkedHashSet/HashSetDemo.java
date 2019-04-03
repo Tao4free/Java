e61bfe10b7f762cb0a2d37c37875914d0f16d03b
tao <tao@tao.my.domain>
2019-04-03 23:01:35 +0900

    test

---
+++ TutorialsPoint/03_Advanced/02_Collections/02-02_The_Collection_Classes/04_LinkedHashSet/HashSetDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HashSetDmo {
+    public static void main(String[] args) {
+        // Create a hash set
+        LinkedHashSet hs = new LinkedHashSet();
+
+        // Add elements to the hash set
+        hs.add("B");
+        hs.add("A");
+        hs.add("D");
+        hs.add("E");
+        hs.add("C");
+        hs.add("F");
+        System.out.println(hs);
+    }
+}
