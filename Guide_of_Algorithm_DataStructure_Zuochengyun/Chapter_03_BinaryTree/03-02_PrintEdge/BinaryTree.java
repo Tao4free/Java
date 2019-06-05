f9f6501474ad8b8ba78398fdcd87f4b192918cb7
tao4free <takizawa_tao@live.com>
2019-06-05 17:48:23 +0900

    test

--- Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/BinaryTree.java
+++ Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/BinaryTree.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class BinaryTree {
     // Root of Binary Tree
     Node root;
+    Node cur;
 
     BinaryTree(int value) {
         root = new Node(value);
@@ -10,24 +13,71 @@ public class BinaryTree {
         root = null;
     }
 
-    private Node addRecursive(Node cur, int value) {
-        if (cur == null) {
-            return new Node(value);
-        }
+    public void add(int parent, int value, int side) {
+        //Node cur = null;
 
-        if (value < cur.value) {
-            cur.left = addRecursive(cur.left, value);
-        } else if (value > cur.value) {
-            cur.right = addRecursive(cur.right, value);
+        if (root == null) {
+            root = new Node(value);
+            return;
         } else {
-            // value already exists
-            return cur;
+            cur = preOrderRecur(root, parent, value);
         }
+        if (cur == null) {
+            return;
+        }
+        //System.out.println("Cur = " + cur.value);
 
-        return cur;
+        if (side == 1 && cur.left == null) {
+            cur.left = new Node(value);
+            //System.out.println("Cur = " + cur.value + " cur.left " + cur.left.value);
+        }
+        if (side == 2 && cur.right == null) {
+            cur.right = new Node (value);
+            //System.out.println("Cur = " + cur.value + " cur.right " + cur.right.value);
+        }
     }
 
-    public void add(int value) {
-        root = addRecursive(root, value);
+    private Node preOrderRecur(Node root, int parent, int value) {
+        //Node cur = null;
+
+        if (root == null) {
+            return null;
+        } else if (root.value == parent) {
+            cur = root;
+            //System.out.println("parent is " + cur.value);
+        }
+        //System.out.println("Test: ");// + cur.value);
+
+        preOrderRecur(root.left, parent, value);
+        preOrderRecur(root.right, parent, value);
+
+        // Method with stack
+        //if (root == null) {
+        //    return null;
+        //}
+
+        //Stack<Node> stack = new Stack<Node>();
+        //stack.add(root);
+        //while ( !stack.isEmpty() ) {
+        //    root = stack.pop();
+        //    if (root.value == value) {
+        //        cur = null;
+        //        break;
+        //    }
+        //    if (root.value == parent) {
+        //        cur = root;
+        //        //System.out.println("parent is " + cur.value);
+        //        break;
+        //    }
+
+        //    if (root.right != null) {
+        //        stack.push(root.right);
+        //    }
+        //    if (root.left != null) {
+        //        stack.push(root.left);
+        //    }
+        //}
+
+        return cur;
     }
 }
--- Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/PrintEdge_1.java
+++ Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/PrintEdge_1.java
@@ -4,12 +4,25 @@ import java.util.*;
 public class PrintEdge_1 {
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
-        for (int i = 1; i <= 16; i++) {
-            bt.add(i);
-        }
+        bt.add(1,1,1);
+        bt.add(1,2,1);
+        bt.add(2,4,2);
+        bt.add(4,7,1);
+        bt.add(4,8,2);
+        bt.add(8,11,2);
+        bt.add(11,13,1);
+        bt.add(11,14,2);
+        bt.add(1,3,2);
+        bt.add(3,5,1);
+        bt.add(3,6,2);
+        bt.add(5,9,1);
+        bt.add(5,10,2);
+        bt.add(9,12,1);
+        bt.add(12,15,1);
+        bt.add(12,16,2);
 
         Node head = bt.root;
-        System.out.println(head.right.value);
+        //System.out.println(head.left.value);
 
         //BinaryTree head = new BinaryTree(1);
         //head.left = new BinaryTree(2);
