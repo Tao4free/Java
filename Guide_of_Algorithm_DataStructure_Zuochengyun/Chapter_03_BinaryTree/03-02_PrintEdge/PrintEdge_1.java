f5ecdc6cafe210b9d6a92810b8e160e08989e86e
tao4free <takizawa_tao@live.com>
2019-06-06 17:31:52 +0900

    test

--- Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/PrintEdge_1.java
+++ Guide_of_Algorithm_DataStructure_Zuochengyun/Chapter_03_BinaryTree/03-02_PrintEdge/PrintEdge_1.java
@@ -22,18 +22,13 @@ public class PrintEdge_1 {
         bt.add(12,16,2);
 
         Node head = bt.root;
-        //System.out.println(head.left.value);
-
-        //BinaryTree head = new BinaryTree(1);
-        //head.left = new BinaryTree(2);
-        //head.left.left = new BinaryTree(4);
-        //head.left.right = new BinaryTree(5);
-        //head.right = new BinaryTree(3);
-        //head.right.left = new BinaryTree(6);
-        //head.right.right = new BinaryTree(7);
 
         System.out.println("Pre-Order recursive:");
         preOrderRecur(head);
+        System.out.println();
+
+        System.out.println("Print edge by rule 1:");
+        printEdge_1(head);
         System.out.println();
 
         //System.out.println("In-Order recursive:");
@@ -55,23 +50,90 @@ public class PrintEdge_1 {
         preOrderRecur(head.right);
     }
 
-    //public static void inOrderRecur(Node head) {
-    //    if (head == null) {
-    //        return;
-    //    }
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
 
-    //    inOrderRecur(head.left);
-    //    System.out.print(head.value + " ");
-    //    inOrderRecur(head.right);
-    //}
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
 
-    //public static void postOrderRecur(Node head) {
-    //    if (head == null) {
-    //        return;
-    //    }
+    public static void postOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
 
-    //    postOrderRecur(head.left);
-    //    postOrderRecur(head.right);
-    //    System.out.print(head.value + " ");
-    //}
+        postOrderRecur(head.left);
+        postOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void printEdge_1(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        //int height = getHeight(head, 0);
+        int height = heightOfTree(head);
+        System.out.println("height is " + height);
+        Node [][] edgeMap = new Node[height][2];
+        setEdgeMap(head, 0, edgeMap);
+
+        // print left edge
+        for (int i = 0; i != edgeMap.length; i++) {
+            System.out.print(edgeMap[i][0].value + " ");
+        }
+
+        // print leaf node exclude left and right edges
+        printLeafNotInMap(head, 0, edgeMap);
+
+        // print right edge exclude left edges
+        for (int i = edgeMap.length - 1; i != -1; i--) {
+            if (edgeMap[i][0] != edgeMap[i][1]) {
+                System.out.print(edgeMap[i][1].value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static int getHeight(Node h, int l) {
+        if (h == null) {
+            return l;
+        }
+        return Math.max(getHeight(h.left, l + 1), getHeight(h.right, l + 1));
+    }
+
+	public static int heightOfTree(Node root) {
+		if (null == root)
+			return 0;
+		int hLeftSub = heightOfTree(root.left);
+		int hRightSub = heightOfTree(root.right);
+        System.out.println("root : " + root.value +  " hLeftSub : " + hLeftSub + " hRightSub : " + hRightSub);
+        int max = Math.max(hLeftSub, hRightSub) + 1;
+		return max;
+    }
+
+    public static void setEdgeMap(Node h, int l, Node[][] edgeMap) {
+        if (h == null) {
+            return;
+        }
+        edgeMap[l][0] = edgeMap[l][0] == null ? h : edgeMap[l][0];
+        edgeMap[l][1] = h;
+        setEdgeMap(h.left, l + 1, edgeMap);
+        setEdgeMap(h.right, l + 1, edgeMap);
+    }
+
+    public static void printLeafNotInMap(Node h, int l, Node[][] m) {
+        if (h == null) {
+            return;
+        }
+
+        if (h.left == null && h.right == null && h != m[l][0] && h != m[l][1]) {
+            System.out.print(h.value + " ");
+        }
+        printLeafNotInMap(h.left, l + 1, m);
+        printLeafNotInMap(h.right, l + 1, m);
+    }
 }
