95c4fd6cffecd18048e408d6c13d725d901bf9dc
Tao4free <tao4free@gmail.com>
2020-06-03 15:08:38 +0900

    testCrateANewProject

---
+++ practice_intelliJ/src/main/java/Stack/Stack.java
@@ -0,0 +1,20 @@
+package Stack;
+
+public class Stack {
+
+    private int size;
+    private int element;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push (int element) {
+        size++;
+    }
+
+    public int pop() {
+        --size;
+        return element;
+    }
+}
